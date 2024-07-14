9a65651868ef9a288295283021204be45558d5f9
khushimurarka <145275086+khushimurarka@users.noreply.github.com>
2024-07-14 22:46:07 +0530

    Add files via upload

---
+++ creatify/src/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.jpg';
+import './App.css';
+import Welcome from './Welcome.jsx';
+import Login from './Login.jsx';
+import Signup from './Signup.jsx'
+
+function App(){ 
+  return (<>
+   
+    
+    <Signup>
+      
+    </Signup>
+
+        </>
+    
+  );
+}
+
+export default App;
